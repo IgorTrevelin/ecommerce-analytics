48be74637f61ecfb19f576456eef9b975bdca529
Vitor Trevelin <vitor.trevelin.xavier.silva@gmail.com>
2024-09-11 15:52:42 -0300

    load_initial_dataset function implementation
    function that loads the raw dataset csv data,
    transform the identifiers to the new standard (as serial),
    and insert all initial data into the dataset tables

--- init_db.py
+++ init_db.py
@@ -1,13 +1,13 @@
-# %%
-
 import os
 import pandas as pd
+from urllib.parse import quote_plus
 
 from sqlalchemy import create_engine, text, Connection
 from dotenv import dotenv_values
 
-env_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), ".env")
-env = dotenv_values(env_file)
+
+def load_csv(filename: str, usecols = None):
+    return pd.read_csv(f"./olist/{filename}", header=0, dtype="str", usecols=usecols)
 
 
 def get_postgress_conn(host: str, port: str, user: str, password: str, db: str):
@@ -15,10 +15,6 @@ def get_postgress_conn(host: str, port: str, user: str, password: str, db: str):
     engine = create_engine(url)
     conn = engine.connect()
     return conn
-
-
-def read_table(conn: Connection, table_name: str):
-    return pd.read_sql(table_name, conn)
 
 
 def insert_batch(conn: Connection, df: pd.DataFrame, table_name: str):
@@ -42,160 +38,137 @@ def insert_batch(conn: Connection, df: pd.DataFrame, table_name: str):
         return False
 
 
-def load_zip_code_prefixes(conn: Connection):
-    df = pd.read_csv(
-        "./olist/olist_geolocation_dataset.csv",
-        header=0,
-        usecols=["geolocation_zip_code_prefix", "geolocation_state"],
-        dtype="str",
-    )
-
-    df = df.groupby("geolocation_zip_code_prefix", as_index=False)[
-        "geolocation_state"
-    ].first()
-
-    df = df.rename(
-        columns={
-            "geolocation_zip_code_prefix": "zip_code_prefix",
-            "geolocation_state": "state",
-        }
-    )
-
-    insert_batch(conn, df, "zip_code_prefixes")
+def clear_database(conn: Connection):
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "schema.sql"), "r") as f:
+        queries = f.read()
+    conn.execute(text(queries))
+    conn.commit()
 
 
-def load_product_categories(conn: Connection):
-    df = pd.read_csv(
-        "./olist/product_category_name_translation.csv",
-        header=0,
-        dtype="str",
-    )
+def load_initial_dataset(conn: Connection):
+    # load data
+    df_categories = load_csv("product_category_name_translation.csv", usecols=["product_category_name", "product_category_name_english"])
+    df_zip = load_csv("olist_geolocation_dataset.csv", usecols=["geolocation_zip_code_prefix", "geolocation_state"])
+    df_products = load_csv("olist_products_dataset.csv", usecols=["product_id", "product_category_name"])
+    df_sellers = load_csv("olist_sellers_dataset.csv", usecols=["seller_id", "seller_zip_code_prefix", "seller_state"])
+    df_customers = load_csv("olist_customers_dataset.csv", usecols=["customer_id", "customer_unique_id", "customer_zip_code_prefix", "customer_state"])
+    df_orders = load_csv("olist_orders_dataset.csv")
+    df_order_items = load_csv("olist_order_items_dataset.csv")
 
-    df = df.drop_duplicates()
+    # rename csv column names to match sql schema
+    df_categories.rename(columns={"product_category_name": "product_category", "product_category_name_english": "product_category_english",}, inplace=True)
+    df_zip.rename(columns={"geolocation_zip_code_prefix": "zip_code_prefix", "geolocation_state": "state"}, inplace=True)
+    df_products.rename(columns={"product_category_name": "product_category"}, inplace=True)
+    df_customers.rename(columns={"customer_zip_code_prefix": "zip_code_prefix", "customer_state": "state"}, inplace=True)
+    df_sellers.rename(columns={"seller_zip_code_prefix": "zip_code_prefix", "seller_state": "state"}, inplace=True)
+    df_orders.rename(columns={
+        "order_status": "status",
+        "order_purchase_timestamp": "purchase_timestamp",
+        "order_approved_at": "approved_at",
+        "order_delivered_carrier_date": "delivered_carrier_date",
+        "order_delivered_customer_date": "delivered_customer_date",
+        "order_estimated_delivery_date": "estimated_delivery_date",
+    }, inplace=True)
 
-    df = df.rename(
-        columns={
-            "product_category_name": "product_category",
-            "product_category_name_english": "product_category_name_english",
-        }
-    )
+    # prepare data to insert into tables
+    # - fix identifiers to be in accordance with the new standard defined in the schema (as serial)
+    complement = df_products[["product_category"]]
+    df_categories = pd.concat([df_categories, complement], axis=0)
+    df_categories.drop_duplicates("product_category", inplace=True)
+    df_categories.fillna({"product_category": "unindentified", "product_category_english": "unindentified"}, inplace=True)
+    # id fix
+    df_categories.sort_values(by="product_category", inplace=True)
+    df_categories.reset_index(drop=True, inplace=True)
+    df_categories["category_id"] = df_categories.index + 1
 
-    insert_batch(conn, df, "product_categories")
+    complement1 = df_customers[["zip_code_prefix", "state"]]
+    complement2 = df_sellers[["zip_code_prefix", "state"]]
+    df_zip = pd.concat([df_zip, complement1, complement2], axis=0)
+    df_zip.drop_duplicates(inplace=True)
+    # id fix
+    df_zip.sort_values(by=["zip_code_prefix", "state"], inplace=True)
+    df_zip.reset_index(drop=True, inplace=True)
+    df_zip["zip_code_id"] = df_zip.index + 1
 
+    df_products.drop_duplicates(inplace=True)
+    df_products.fillna({"product_category": "unindentified"}, inplace=True)
+    df_products = df_products.merge(df_categories, on="product_category", how="left")
+    df_products.drop(columns=["product_category", "product_category_english"], inplace=True)
+    # id fix
+    df_products.sort_values(by="product_id", inplace=True)
+    df_products.reset_index(drop=True, inplace=True)
+    df_products["pid"] = df_products.index + 1
 
-def load_products(conn: Connection):
-    df_cats = read_table(conn, "product_categories")
+    df_customers.drop_duplicates(inplace=True)
+    df_customers = df_customers.merge(df_zip, on=["zip_code_prefix", "state"], how="left")
+    df_customers.drop(columns=["zip_code_prefix", "state"], inplace=True)
+    # id fix
+    df_customers.sort_values(by="customer_id", inplace=True)
+    df_customers.reset_index(drop=True, inplace=True)
+    df_customers["cid"] = df_customers.index + 1
 
-    df = pd.read_csv(
-        "./olist/olist_products_dataset.csv",
-        header=0,
-        usecols=["product_category_name"],
-        dtype="str",
-    )
+    df_sellers.drop_duplicates(inplace=True)
+    df_sellers = df_sellers.merge(df_zip, on=["zip_code_prefix", "state"], how="left")
+    df_sellers.drop(columns=["zip_code_prefix", "state"], inplace=True)
+    # id fix
+    df_sellers.sort_values(by="seller_id", inplace=True)
+    df_sellers.reset_index(drop=True, inplace=True)
+    df_sellers["sid"] = df_sellers.index + 1
 
-    df = df.merge(
-        df_cats,
-        left_on="product_category_name",
-        right_on="product_category",
-        how="inner",
-    )
+    df_orders.fillna({"approved_at": "2000-01-01 00:00:00", "delivered_carrier_date": "2000-01-01 00:00:00", "delivered_customer_date": "2000-01-01 00:00:00"}, inplace=True)
+    df_orders.drop_duplicates(inplace=True)
+    df_orders = df_orders.merge(df_customers, on="customer_id", how="left")
+    df_orders.drop(columns=["customer_id", "customer_unique_id", "zip_code_id"], inplace=True)
+    # id fix
+    df_orders.sort_values(by="order_id", inplace=True)
+    df_orders.reset_index(drop=True, inplace=True)
+    df_orders["oid"] = df_orders.index + 1
 
-    df = df[["category_id"]]
+    df_order_items.drop_duplicates(inplace=True)
+    df_order_items = df_order_items \
+        .merge(df_orders[["order_id", "oid"]], on="order_id", how="left") \
+        .merge(df_products[["product_id", "pid"]], on="product_id", how="left") \
+        .merge(df_sellers[["seller_id", "sid"]], on="seller_id", how="left")
+    df_order_items.drop(columns=["order_id", "product_id", "seller_id"], inplace=True)
+    # id fix
+    df_order_items.sort_values(by="order_item_id", inplace=True)
+    df_order_items.reset_index(drop=True, inplace=True)
 
-    insert_batch(conn, df, "products")
+    # clean tmp columns
+    df_categories.drop(columns=["category_id"], inplace=True)
+    df_zip.drop(columns=["zip_code_id"], inplace=True)
+    df_products.drop(columns=["product_id", "pid"], inplace=True)
+    df_customers.drop(columns=["customer_id", "cid"], inplace=True)
+    df_sellers.drop(columns=["seller_id", "sid"], inplace=True)
+    df_orders.drop(columns=["order_id", "oid"], inplace=True)
+    df_orders.rename(columns={"cid": "customer_id"}, inplace=True)
+    df_order_items.rename(columns={"oid": "order_id", "pid": "product_id", "sid": "seller_id"}, inplace=True)
 
+    # insert data to database
+    for table_name, df in {
+        "product_categories": df_categories,
+        "zip_code_prefixes": df_zip,
+        "products": df_products,
+        "customers": df_customers,
+        "sellers": df_sellers,
+        "order_items": df_order_items,
+        "orders": df_orders,
+    }.items():
+        insert_batch(conn, df, table_name)
 
-def load_customers(conn: Connection):
-    df_zips = read_table(conn, "zip_code_prefixes")
-
-    df = pd.read_csv(
-        "./olist/olist_customers_dataset.csv",
-        header=0,
-        usecols=["customer_zip_code_prefix"],
-        dtype="str",
-    )
-
-    df = df.merge(
-        df_zips,
-        left_on="customer_zip_code_prefix",
-        right_on="zip_code_prefix",
-        how="inner",
-    )
-    df = df[["zip_code_id"]]
-
-    insert_batch(conn, df, "customers")
-
-
-def load_sellers(conn: Connection):
-    df_zips = read_table(conn, "zip_code_prefixes")
-
-    df = pd.read_csv(
-        "./olist/olist_sellers_dataset.csv",
-        header=0,
-        usecols=["seller_zip_code_prefix"],
-        dtype="str",
-    )
-
-    df = df.merge(
-        df_zips,
-        left_on="seller_zip_code_prefix",
-        right_on="zip_code_prefix",
-        how="inner",
-    )
-    df = df[["zip_code_id"]]
-
-    insert_batch(conn, df, "sellers")
-
-
-def load_csv(filename: str):
-    return pd.read_csv(f"./olist/{filename}", header=0, dtype="str")
-
-
-df_cats = load_csv("product_category_name_translation.csv")
-df_zip = (
-    load_csv("olist_geolocation_dataset.csv")
-    .groupby("geolocation_zip_code_prefix", as_index=False)["geolocation_state"]
-    .first()
-)
-df_prods = load_csv("olist_products_dataset.csv")
-df_sellers = load_csv("olist_sellers_dataset.csv")
-df_customers = load_csv("olist_customers_dataset.csv")
-df_orders = load_csv("olist_orders_dataset.csv")
-df_order_items = load_csv("olist_order_items_dataset.csv")
-
-df = (
-    df_order_items.merge(df_orders, on="order_id", how="inner")
-    .merge(df_customers, on="customer_id", how="inner")
-    .merge(df_sellers, on="seller_id", how="inner")
-    .merge(df_prods, on="product_id", how="inner")
-    .merge(df_cats, on="product_category_name", how="inner")
-    .merge(
-        df_zip.rename(columns={c: f"customer_{c}" for c in df_zip.columns}),
-        left_on="customer_zip_code_prefix",
-        right_on="customer_geolocation_zip_code_prefix",
-        how="left",
-    )
-    .merge(
-        df_zip.rename(columns={c: f"seller_{c}" for c in df_zip.columns}),
-        left_on="seller_zip_code_prefix",
-        right_on="seller_geolocation_zip_code_prefix",
-        how="left",
-    )
-)
-df.head()
 
 if __name__ == "__main__":
+    env_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), ".env")
+    env = dotenv_values(env_file)
+
     host = env.get("POSTGRES_HOST", "127.0.0.1")
     port = env.get("POSTGRES_PORT", "5432")
     user = env.get("POSTGRES_USER", "root")
     password = env.get("POSTGRES_PASSWORD", "postgres")
     db = env.get("POSTGRES_DB", "postgres")
 
-    conn = get_postgress_conn(host, port, user, password, db)
-    load_zip_code_prefixes(conn)
-    load_product_categories(conn)
-    load_products(conn)
-    load_customers(conn)
-    load_sellers(conn)
+    conn = get_postgress_conn(host, port, user, quote_plus(password), db)
+    clear_database(conn)
+    load_initial_dataset(conn)
 
     conn.close()
